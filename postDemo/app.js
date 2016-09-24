f98a7fe14f4205d633f2ecac20b6dd1c2aaa1576
Tajammul Pasha <tajammulpashazuffari@gmail.com>
2016-09-24 14:02:03 +0000

    POSTman

---
+++ postDemo/app.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+var friends = ["salman", "abuzar", "jawad", "abrar", "arun", "nandeep"];
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/friends", function(req,res){
+    
+    res.render("friends",{frnds : friends});
+});
+
+app.post("/addfriend", function(req, res){
+    var newFriend = req.body.newfriend;
+    friends.push(newFriend);
+    res.redirect("/friends");
+    
+});
+
+
+app.get("*", function(req, res){
+    res.send("Go and get some life");
+});
+
+
+app.listen(process.env.PORT, process.env.IP,function(){
+    console.log("Server has started at port number "+ process.env.PORT);
+});
+
